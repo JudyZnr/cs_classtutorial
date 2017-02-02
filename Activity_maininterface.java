7d3e8ceb6b47adf9c9084cb900f7ba77835aec8c
JudyZnr <JudyZnr@gmail.com>
2017-02-01 19:45:06 -0500

    first commit

---
+++ Activity_maininterface.java
@@ -0,0 +1,18 @@
+package com.example.administorzz.cs_classtutorial;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by administorzz on 17/2/1.
+ */
+
+public class Activity_maininterface extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maininterface);
+       System.out.println("hello");
+
+    }
+}
