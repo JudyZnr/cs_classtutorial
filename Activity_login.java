8cf4dbb40943a31798b7eb867e652c1b47bf7e17
JudyZnr <JudyZnr@gmail.com>
2017-02-03 02:35:47 -0500

    Android_lab2b

---
+++ Activity_login.java
@@ -0,0 +1,32 @@
+package com.example.administorzz.cs_classtutorial;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Activity_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button button = (Button) findViewById(R.id.jump_btn);
+
+        final View.OnClickListener btn_click = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+             launch_maininterface();
+                //click motion
+            }
+        };
+        button.setOnClickListener(btn_click);
+    }
+
+    public void launch_maininterface(){
+        Intent intent = new Intent(this,Activity_maininterface.class);
+        startActivity(intent);
+    }
+}
---
+++ Activity_splash.java
@@ -0,0 +1,32 @@
+package com.example.administorzz.cs_classtutorial;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by administorzz on 17/2/3.
+ */
+
+public class Activity_splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Thread myThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(), Activity_login.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+}
